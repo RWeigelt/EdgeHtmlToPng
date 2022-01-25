eddc36a3654aa5a975205da5342f4c6f4abcc2f9
Roland Weigelt <mail@roland-weigelt.de>
2022-01-25 21:36:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+var source1 = "http://www.example.com";
+var source2 = Path.Combine(AppContext.BaseDirectory, "Example2.html");
+
+var outputFilePath1 = Path.Combine(desktopPath, "Example1.png");
+var outputFilePath2 = Path.Combine(desktopPath, "Example2.png");
+
+HtmlToPng(source1, outputFilePath1);
+HtmlToPng(source2, outputFilePath2);
+
+return;
+
+void HtmlToPng(string source, string outputFilePath)
+{
+    var arguments = $"--headless --disable-gpu --default-background-color=00000000 --window-size=1920,1080 --screenshot=\"{outputFilePath}\" \"{source}\"";
+
+    var process = new System.Diagnostics.Process();
+    process.StartInfo.FileName = "msedge";
+    process.StartInfo.Arguments = arguments;
+    process.StartInfo.UseShellExecute = true;
+    process.Start();
+}
